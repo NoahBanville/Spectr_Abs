c2718c20669f1e175f77533811d0efe68a5ab5c7
NoahBanville <127630930+NoahBanville@users.noreply.github.com>
2024-08-19 22:12:24 -0400

    Add files via upload

---
+++ spectrum_graph.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+plt.figure(figsize=(8,6))
+# chemin d'accès à changer selon utilisateur
+repertoire_csv = r"C:\Users\noah\Downloads\test"
+
+#boucle sur nombre CSV
+for fichier in os.listdir(repertoire_csv):
+    if fichier.endswith(".csv"):
+        
+        # Lecture du fichier CSV
+        df = pd.read_csv(os.path.join(repertoire_csv, fichier), skiprows=13)
+        
+        x = df['WL(nm)']
+        y = df['Abs.']
+        
+        plt.plot(x, y, label=fichier.split('-')[0])
+
+# mise en forme
+plt.title("Spectre d'absorbance", fontsize=20)
+plt.xlabel("Longueur d'onde (nm)")
+plt.ylabel('Absorbance')
+plt.legend()
+
+plt.show()
